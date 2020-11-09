3b14cbcad71768535055ceac6e51a63b3af2c09c
Dean Hadzi <deanhadzi82@gmail.com>
2020-11-08 22:40:12 -0500

    EX1 complete.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,23 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # creating an empty hashtable.
+    weights_dict = {}
 
+    for index, weight in enumerate(weights):
+        
+        # check if we have limit - weight in the dict already.
+        if limit - weight in weights_dict:
+
+            # find the index of the value which is in the dict already.
+            second_item = weights_dict.get(limit - weight), index
+            
+            # return the solution.
+            return tuple(sorted(second_item, reverse=True))
+
+        # otherwise, if item is not in hashtable, add it.
+        else:
+            weights_dict[weight] = index
+
+    # return None if we didn't find anything.
     return None
